43e8ac781c595006d7b40ae7a611359021341dea
GeorgeTheGGamer <george.blagden634@gmail.com>
2024-06-22 23:35:35 +0100

    Update webScraping.py

--- webScraping.py
+++ webScraping.py
@@ -15,3 +15,12 @@ soup = BeautifulSoup(data.text)
 #passing in our html data to the beautiful soup class 
 
 standings_table = soup.select()
+
+links = standings_table.find_all('a')
+#Using findall instead of select, select using css selectors and find all only finds tags
+#want to get the href property of each link
+#The href attribute specifies the URL of the page the link goes to
+
+#Make a list comprehension, remember from haskell
+links = [l.get("href") for l in links]
+#Now have the href for every links
